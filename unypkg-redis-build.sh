8ba948e98160c0fc7a7dc5c1fd9618e74f438ba6
michacassola <mich@casso.la>
2024-09-07 14:57:46 +0200

    Update unypkg-redis-build.sh

--- unypkg-redis-build.sh
+++ unypkg-redis-build.sh
@@ -11,7 +11,7 @@ set -vx
 wget -qO- uny.nu/pkg | bash -s buildsys
 
 ### Installing build dependencies
-#unyp install python expat openssl
+unyp install openssl
 
 #pip3_bin=(/uny/pkg/python/*/bin/pip3)
 #"${pip3_bin[0]}" install --upgrade pip
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="redis"
-pkggit="https://github.com/redis/redis.git refs/tags/*"
+pkggit="https://github.com/redis/redis.git refs/tags/[0-9.]*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "/[0-9.]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "/[0-9.].*" | sed "s|/||")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -77,12 +77,9 @@ get_include_paths
 
 unset LD_RUN_PATH
 
-./configure \
-    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
+make PREFIX=/uny/pkg/"$pkgname"/"$pkgver" BUILD_TLS=yes -j"$(nproc)"
 
-make -j"$(nproc)"
-make -j"$(nproc)" check 
-make -j"$(nproc)" install
+make PREFIX=/uny/pkg/"$pkgname"/"$pkgver" -j"$(nproc)" install
 
 ####################################################
 ### End of individual build script
