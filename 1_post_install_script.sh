a1f883b9a1c7d9501dc9bddf1581ad1bf6a76065
michacassola <mich@casso.la>
2024-09-07 15:52:21 +0200

    Update 1_post_install_script.sh

--- 1_post_install_script.sh
+++ 1_post_install_script.sh
@@ -13,6 +13,8 @@ cd "$unypkg_root_dir" || exit
 mkdir -pv /etc/uny/redis
 cp -a etc/redis.conf /etc/uny/redis/
 
+mkdir -pv /var/lib/redis
+
 cp -a utils/systemd-redis_server.service /etc/systemd/system/uny-redis-server.service
 sed "s|--supervised systemd --daemonize no|/etc/uny/redis/redis.conf|" -i /etc/systemd/system/uny-redis-server.service
 sed -e '/\[Install\]/a\' -e 'Alias=redis-server.service redis.service' -i /etc/systemd/system/uny-redis-server.service
